54fa38465d5909fcd3f2fdf29a16e9c978e01f71
David Trudgian <david.trudgian@utsouthwestern.edu>
2017-08-24 12:26:28 -0500

    Missing API version in test API_URL

--- tests/test_clair.py
+++ tests/test_clair.py
@@ -2,7 +2,7 @@ import pytest
 
 from clair_singularity.clair import check_clair, post_layer, get_report
 
-API_URL = 'http://127.0.0.1:6060/'
+API_URL = 'http://127.0.0.1:6060/v1/'
 
 def test_check_clair():
     # We can talk to the API
