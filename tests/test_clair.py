91f37cdfa5265975b4169e88a23dafbe8d4bd044
David Trudgian <david.trudgian@utsouthwestern.edu>
2017-08-24 11:11:48 -0500

    API simple connectivity test

---
+++ tests/test_clair.py
@@ -0,0 +1,10 @@
+import pytest
+
+from clair_singularity.clair import check_clair, post_layer, get_report
+
+API_URL = 'http://127.0.0.1:6060'
+
+def test_check_clair:
+    # We can talk to the API
+    assert check_clair(API_URL)
+
